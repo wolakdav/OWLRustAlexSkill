d082b9d6cf34b9620c8b6847e45cce849115ac2f
David <awesomesauce712@gmail.com>
2019-05-27 13:40:30 -0700

    Adding match stuff

--- src/main.rs
+++ src/main.rs
@@ -56,6 +56,7 @@ fn getCurrentMatch(liveMatch:json::JsonValue) -> String{
         }
       
     }
+    getNextMatch();
     return result
 }
 
@@ -71,7 +72,7 @@ fn getTodaysNextMatch(nextMatch:json::JsonValue,rankings:json::JsonValue) -> Str
     return format!("The next match will be between {}(League Rank {}) and {}(League Rank {})",nextFirstContendor.to_string(),firstContendorRanking.to_string(),nextSecondContendor.to_string(),secondContendorRanking.to_string());
     }else{
 
-        return format!("There are no more matches today,check back in later");
+        return format!("There are no more matches today,check back in tomorrow!");
     }
 }
 fn getRanking(team: String, rankings:json::JsonValue ) -> usize
@@ -104,6 +105,100 @@ fn convertMilliSeconds(timeToNext: f64) -> String {
     }
 }
 
-fn getNextMatch()-> String{
-      let liveMatchData = json::parse(&(reqwest::get("https://api.overwatchleague.com/schedule")?.text()?).to_string()).unwrap();
+fn getNextMatch()->  Result<(), Box<std::error::Error>>{
+      let schedule = json::parse(&(reqwest::get("https://api.overwatchleague.com/schedule")?.text()?).to_string()).unwrap();
+      let currentStage = getCurrentStage(schedule["data"]["stages"].clone());
+      let matchId = getNextMatchInSchedule(schedule["data"]["stages"][currentStage].clone());
+      println!("{}",matchId);
+      Ok(())
+      
+}
+
+
+fn getNextMatchInSchedule(stage:json::JsonValue)->String{
+    let matches = &stage["matches"];
+    let numberOfMatches = matches.len();
+     for mut i in 0..numberOfMatches
+    {
+        if(matches[i+3]["state"] == "CONCLUDED")//All previous matches for that day are concluded
+        {
+            i = i + 3;
+        }else{
+            
+            if(matches[i]["state"] == "PENDING")
+            {
+                let firstContendor =  (&matches[i]["competitors"][0]["name"]).to_string();
+                let secondContendor = (&matches[i]["competitors"][1]["name"]).to_string();
+                let mut startDate = (&matches[i]["startDate"]).to_string();
+                if (startDate != "null") && (firstContendor != "null") & (secondContendor != "null")
+                {
+                    startDate = formateDate(startDate);
+                    return format!("The next match is {} vs {} on {} ",firstContendor,secondContendor,startDate);
+                }else{
+                    return "Error getting new match information".to_string();
+                }
+            }
+        }
+    }
+    return "Failed to find matchId".to_string();;
+}
+
+
+fn formateDate(mut date:String )->String{
+    date = date.replacen("-"," ",3);
+    date = date.replacen("T"," ",1);
+    let foo:Vec<&str> = date.split(" ").collect();
+    let mut month =  String::new();
+    month = getMonth(foo[1].to_string());
+    let day = formatDay(foo[2].to_string());
+    let year = foo[0].to_string();
+    return format!("{} {}, {}",month,day,year);
+}
+
+fn formatDay(day:String) -> String{
+    match day.as_ref()
+    {
+        "01" => "1st".to_string(),
+        "02" => "2nd".to_string(),
+        "03" =>  "3rd".to_string(),
+        "04" => "4th".to_string(),
+        "05" => "5th".to_string(),
+        "06" => "6th".to_string(),
+        "07" => "7th".to_string(),
+        "08" => "8th".to_string(),
+        "9" => "9th".to_string(),
+        _ => (day+"th").to_string(),
+            }
+}
+fn getMonth(month:String) -> String {
+    match month.as_ref()
+    {
+        "01" =>  "January",
+        "02" =>  "February",
+        "03" =>  "March",
+        "04" => "April",
+        "05" => "May",
+        "06" => "June",
+        "07" =>  "July",
+        "08" =>  "September",
+        "09" => "September",
+        "10" =>  "October",
+        "11" =>"November",
+        "12" =>  "December",
+        _ => "Unknown month Error",
+    }.to_string()
+}
+
+fn getCurrentStage(stages:json::JsonValue)->usize{
+    for i in 0..4
+    {
+        let stageToCheck = &stages[i];
+        let stageMatchesAmount = stageToCheck["matches"].len() ;
+        let lastMatchConclussion = &stageToCheck["matches"][stageMatchesAmount - 1]["state"];
+        if(lastMatchConclussion != "CONCLUDED")
+        {
+            return i;
+        }
+    }
+    return 1000000;
 }
