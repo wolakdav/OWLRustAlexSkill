e4427f23ab68a82614d03f7410d5f5f6dd05af37
David <awesomesauce712@gmail.com>
2019-06-04 13:45:53 -0700

    Finished up program and added some basic tests

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,67 @@
 extern crate chrono;
 extern crate json;
 extern crate reqwest;
-use std::io;
 use std::collections::HashMap;
-
+use std::io;
 
 fn main() -> Result<(), Box<std::error::Error>> {
-
     println!("Welcome to the Overwatch League Tracker! While we're unfornately still terminal based theres plan to change that soon so stay tuned!");
     println!("--------------");
     printCurrentAndNextMatch();
     let mut userChoice = 1000000000;
-    while userChoice == 1000000000
-    {
-    userChoice = menuChoice();
-    }
-    match userChoice {
-        1 => {
-            let nextMatch = getNextMatch().unwrap();
-            println!("{}",nextMatch);
-            },
-        2 => {let teamInfo = getTeamInfo().unwrap();
-            println!("{}",teamInfo);}
-        3 => {let rankings = getAllRankings().unwrap();
-            println!("{}",rankings);
+    while userChoice != 4 {
+        while userChoice == 1000000000 {
+            userChoice = menuChoice();
+        }
+        match userChoice {
+            1 => {
+                let nextMatch = getNextMatch().unwrap();
+                println!("{}", nextMatch);
             }
-        _ => println!("oh god oh fuck"),
-    };
-
+            2 => {
+                let teamInfo = getTeamInfo().unwrap();
+                println!("{}", teamInfo);
+            }
+            3 => {
+                let rankings = getAllRankings().unwrap();
+                println!("{}", rankings);
+            }
+            4 => {
+                let userChoice = 4;
+            }
+            _ => println!("Error:Invalid selections"),
+        };
+    }
     Ok(())
 }
 
-fn getAllRankings() ->  Result<(String),Box<std::error::Error>> {
-
-         let mut rankings = json::parse(
+fn getAllRankings() -> Result<(String), Box<std::error::Error>> {
+    let mut rankings = json::parse(
         &(reqwest::get("https://api.overwatchleague.com/ranking")?.text()?).to_string(),
     )
     .unwrap();
-    
+
     let rankings = rankings["content"].clone();
     let amtCompetitors = rankings.len();
     let mut result = "Current Overwatch League Rankings:".to_string();
     let mut teamName = String::new();
     let mut rank = String::new();
-       for mut i in 0..amtCompetitors 
-    {
+    for mut i in 0..amtCompetitors {
         teamName = rankings[i]["competitor"]["name"].to_string();
         rank = rankings[i]["placement"].to_string();
-        result = format!("{}\n {}:  Rank {}",result,teamName,rank);
+        result = format!("{}\n {}:  Rank {}", result, teamName, rank);
     }
-     Ok(result)
+    Ok(result)
 }
 
-fn getTeamInfo() -> Result<(String),Box<std::error::Error>> {
-
-    let teamIDs = [7698,4402,7692,4523,4407,7699,7693,4525,4410,4406,4405,4403,7694,4524,4404,4409,4408,7695,7696,7697];
-     println!("Select which team you like to know more about:\n");
-println!("
+fn getTeamInfo() -> Result<(String), Box<std::error::Error>> {
+    let teamIDs = [
+        7698, 4402, 7692, 4523, 4407, 7699, 7693, 4525, 4410, 4406, 4405, 4403, 7694, 4524, 4404,
+        4409, 4408, 7695, 7696, 7697,
+    ];
+    println!("Select which team you like to know more about:\n");
+    println!(
+        "
     1. Atlanta Reign
     2.Boston Uprising	
     3.Chengdu Hunters 
@@ -76,25 +81,28 @@ println!("
     17.Shanghai Dragons	
     18.Toronto Defiant	
     19.Vancouver Titans	
-    20.Washington Justice" ); 
+    20.Washington Justice"
+    );
     let mut apiURL = String::new();
-       let mut input_text = String::new();
+    let mut input_text = String::new();
     io::stdin()
         .read_line(&mut input_text)
         .expect("failed to read from stdin");
 
     let trimmed = input_text.trim();
     let input = match trimmed.parse::<u32>() {
-        Ok(i) =>  i,
-        Err(..) =>  1000000000,
+        Ok(i) => i,
+        Err(..) => 1000000000,
     };
-    if input < 0 || input > 20 
-    {
+    if input < 0 || input > 20 {
         return Ok("That is not a valid team".to_string());
     }
-        apiURL = format!("https://api.overwatchleague.com/teams/{}",teamIDs[(input-1) as usize].to_string());//This always give back the same data so this function is bugged =/, looks like ranking call has the correct stats thou so that might be worth a look
-    
-  let teamInfo = json::parse(&(reqwest::get(&apiURL[..])?.text()?).to_string(), ).unwrap();
+    apiURL = format!(
+        "https://api.overwatchleague.com/teams/{}",
+        teamIDs[(input - 1) as usize].to_string()
+    ); //This always give back the same data so this function is bugged =/, looks like ranking call has the correct stats thou so that might be worth a look
+
+    let teamInfo = json::parse(&(reqwest::get(&apiURL[..])?.text()?).to_string()).unwrap();
     let teamName = teamInfo["description"].to_string();
     let teamRank = getRanking(teamName.clone()).unwrap();
     let matchWins = teamInfo["ranking"]["matchWin"].to_string();
@@ -104,15 +112,17 @@ println!("
     let gameLoss = teamInfo["ranking"]["gameLoss"].to_string();
     let gameTie = teamInfo["ranking"]["gameTie"].to_string();
 
-    let result = format!("{} is currently {} place in the league.
+    let result = format!(
+        "{} is currently {} place in the league.
     During the current/last stage they had {} match wins,{} match losses, and {} match ties.
-    During that time they had won {} maps, lost {} maps, and tied on {} maps",teamName,teamRank,matchWins,matchLoss,matchDraw,gameWin,gameLoss,gameTie);
+    During that time they had won {} maps, lost {} maps, and tied on {} maps",
+        teamName, teamRank, matchWins, matchLoss, matchDraw, gameWin, gameLoss, gameTie
+    );
 
     Ok(result)
 }
 
-fn printCurrentAndNextMatch() -> Result<(),Box<std::error::Error>>
-{
+fn printCurrentAndNextMatch() -> Result<(), Box<std::error::Error>> {
     let liveMatchData = json::parse(
         &(reqwest::get("https://api.overwatchleague.com/live-match")?.text()?).to_string(),
     )
@@ -142,8 +152,8 @@ fn menuChoice() -> u32 {
 
     let trimmed = input_text.trim();
     match trimmed.parse::<u32>() {
-        Ok(i) =>  i,
-        Err(..) =>  1000000000,
+        Ok(i) => i,
+        Err(..) => 1000000000,
     }
 }
 
@@ -159,7 +169,7 @@ fn getCurrentMatch(liveMatch: json::JsonValue) -> String {
     let mut currentWinner = String::new();
     let mut leaderScore = String::new();
     let mut loserScore = String::new();
- 
+
     if currentState == "LIVE" {
         if firstContendorScore == secondContendorScore {
             currentWinner = ("the teams being tied").to_string();
@@ -191,7 +201,7 @@ fn getCurrentMatch(liveMatch: json::JsonValue) -> String {
         }
     }
     let nextMatch = getNextMatch().unwrap();
-    return format!("{} \n {} ",result,nextMatch);
+    return format!("{} \n {} ", result, nextMatch);
 }
 
 fn getTodaysNextMatch(nextMatch: json::JsonValue) -> String {
@@ -213,8 +223,7 @@ fn getTodaysNextMatch(nextMatch: json::JsonValue) -> String {
     }
 }
 fn getRanking(team: String) -> Result<(usize), Box<std::error::Error>> {
-
-     let rankings = json::parse(
+    let rankings = json::parse(
         &(reqwest::get("https://api.overwatchleague.com/ranking")?.text()?).to_string(),
     )
     .unwrap();
@@ -234,13 +243,21 @@ fn getRanking(team: String) -> Result<(usize), Box<std::error::Error>> {
 }
 
 fn convertMilliSeconds(timeToNext: f64) -> String {
-    let mut converted = timeToNext / (3.6 * (1000000.0));
-    let hours = converted.floor();
-    let minutes = 60.0 * (converted - hours);
-    if (hours > 1.0) {
-        format!("{:.0} hours and {:.0} minutes", hours, minutes)
+    if timeToNext > 60000.0 {
+        let mut converted = timeToNext / (3.6 * (1000000.0));
+        let hours = converted.floor();
+        let minutes = 60.0 * (converted - hours);
+        if (hours > 1.0) {
+            format!("{:.0} hours and {:.0} minutes", hours, minutes)
+        } else {
+            format!("{:.0} minutes, time to get hyped!", minutes)
+        }
+    } else if (timeToNext <= 60000.0) && (timeToNext >= 0.0) {
+        format!("Match is starting in less then a minute!")
+    } else if timeToNext < 0.0 {
+        format!("A error has occured getting the time until next match")
     } else {
-        format!("{:.0} minutes, time to get hyped!", minutes)
+        format!("The match has started!")
     }
 }
 
@@ -271,11 +288,15 @@ fn getNextMatchInSchedule(stage: json::JsonValue) -> String {
                 let mut startDate = (&matches[i]["startDate"]).to_string();
                 if (startDate != "null") && (firstContendor != "null") & (secondContendor != "null")
                 {
-                    startDate = formateDate(startDate);
-                   
+                    startDate = formatDate(startDate);
+
                     return format!(
                         "The next match is {}(League Rank {}) vs {}(League Rank {}) on {} ",
-                        firstContendor, firstContendorRanking,secondContendor,secondContendorRanking, startDate
+                        firstContendor,
+                        firstContendorRanking,
+                        secondContendor,
+                        secondContendorRanking,
+                        startDate
                     );
                 } else {
                     return "Error getting new match information".to_string();
@@ -286,7 +307,7 @@ fn getNextMatchInSchedule(stage: json::JsonValue) -> String {
     return "Failed to find matchId".to_string();;
 }
 
-fn formateDate(mut date: String) -> String {
+fn formatDate(mut date: String) -> String {
     date = date.replacen("-", " ", 3);
     date = date.replacen("T", " ", 1);
     let foo: Vec<&str> = date.split(" ").collect();
@@ -339,4 +360,60 @@ fn getCurrentStage(stages: json::JsonValue) -> usize {
         }
     }
     return 1000000;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::convertMilliSeconds;
+    use crate::formatDate;
+    use crate::formatDay;
+    use crate::getMonth;
+    #[test]
+    fn test_milli_second_conversion() {
+        let valid_time = convertMilliSeconds(1002313231.0);
+        let valid_time_2 = convertMilliSeconds(30000.0);
+        let valid_time_3 = convertMilliSeconds(1.0);
+        let valid_time_4 = convertMilliSeconds(0.0);
+        let invalid_time = convertMilliSeconds(-10231.0);
+        assert_eq!(valid_time, "278 hours and 25 minutes");
+        assert_eq!(valid_time_2, "Match is starting in less then a minute!");
+        assert_eq!(valid_time_3, "Match is starting in less then a minute!");
+        assert_eq!(valid_time_4, "Match is starting in less then a minute!");
+        assert_eq!(
+            invalid_time,
+            "A error has occured getting the time until next match"
+        );
+    }
+
+    #[test]
+    fn format_date_test() {
+        let valid_time_stamp = formatDate("2019-02-15T00:00:00.000Z".to_string());
+        let invald_time_stamp = formatDate("Invalid TimeStamp".to_string());
+        assert_eq!(valid_time_stamp, "February 15th, 2019");
+        assert_eq!(invald_time_stamp, "Unknown month Error imeStampth, Invalid");
+    }
+
+    #[test]
+    fn format_day_test() {
+        let first = formatDay("01".to_string());
+        let second = formatDay("02".to_string());
+        let third = formatDay("03".to_string());
+        let seventh = formatDay("07".to_string());
+        let thirtyth = formatDay("30".to_string());
+        assert_eq!(first, "1st".to_string());
+        assert_eq!(second, "2nd".to_string());
+        assert_eq!(third, "3rd".to_string());
+        assert_eq!(seventh, "7th".to_string());
+        assert_eq!(thirtyth, "30th".to_string());
+    }
+
+    #[test]
+    fn test_get_month() {
+        let jan = getMonth("01".to_string());
+        let july = getMonth("07".to_string());
+        let invalid_month = getMonth("123210".to_string());
+        assert_eq!(jan, "January".to_string());
+        assert_eq!(july, "July".to_string());
+        assert_eq!(invalid_month, "Unknown month Error".to_string());
+    }
 }
